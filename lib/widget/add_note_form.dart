be763041535d81491b48e0fdf1fa7ada2373e28d
smgr9 <mohamadgazer@gmail.com>
2023-06-21 19:20:27 +0300

    add delet method

--- lib/widget/add_note_form.dart
+++ lib/widget/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:note/Model/note_model.dart';
 import 'package:note/cubit/Add_note_cubit/add_note_cubit.dart';
 import 'package:note/widget/custom_button.dart';
@@ -52,11 +53,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
                   onTap: () {
                     if (formKey.currentState!.validate()) {
                       formKey.currentState!.save();
-
+                      var currentDate = DateTime.now();
+                      var formatedCurrentDate =
+                          DateFormat("dd-M-yyyy").format(currentDate);
                       var noteModel = NoteModel(
                           title: title!,
                           subtitle: subtitle!,
-                          date: DateTime.now().toString(),
+                          date: formatedCurrentDate.toString(),
                           color: Colors.blue.value);
                       BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
                     } else {
